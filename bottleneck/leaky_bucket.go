2973a8228a5f90f5ef634faf9d492c67165fe715
Alex Dyukov <alex.dyukov.93@gmail.com>
2025-08-10 19:49:25 +0300

    Fix: add leaky and token bucket algorithms

---
+++ bottleneck/leaky_bucket.go
@@ -0,0 +1,16 @@
+package bottleneck
+
+// LeakyBucket typed Bottleneck implements leaky bucket algorithm.
+type LeakyBucket Regular
+
+// NewLeakyBucket returns leaky bucket algo implementation of Bottleneck.
+func NewLeakyBucket(size int) (*LeakyBucket, error) {
+	bn, err := NewRegular(size, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := LeakyBucket(*bn)
+
+	return &ret, nil
+}
