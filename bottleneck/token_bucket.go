d93b49872864aa685f4f8a5e1d58bb28fcfd84c8
Alex Dyukov <alex.dyukov.93@gmail.com>
2025-08-10 19:49:25 +0300

    Fix: add leaky and token bucket algorithms

---
+++ bottleneck/token_bucket.go
@@ -0,0 +1,18 @@
+package bottleneck
+
+// TokenBucket typed Bottleneck similar to ticker queue.
+// Provides bottle neck which pass 1 and no more then each {time.Second/RPS} second.
+// RPS 1000 means pass 1 each 1 millisecond.
+type TokenBucket Equalizer
+
+// NewTokenBucket returns token bucket algo implementation of Bottleneck.
+func NewTokenBucket(rps, burst int) (*TokenBucket, error) {
+	bn, err := NewEqualizer(rps, burst)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := TokenBucket(*bn)
+
+	return &ret, nil
+}
